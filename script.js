ba72ea9c329506d6a4852580ab8a5a6c7f53564f
Priyadarshi Nayak <priyadarshinayak2001@gmail.com>
2026-05-20 23:38:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const message = document.getElementById("message");
+
+    if (username === "admin" && password === "1234") {
+        message.style.color = "green";
+        message.innerText = "Login Successful!";
+    } else {
+        message.style.color = "red";
+        message.innerText = "Invalid Username or Password!";
+    }
+});
